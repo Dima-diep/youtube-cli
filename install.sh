74c809b37ddcd322582a4ce8c060c136f0b5f0eb
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2022-05-30 09:53:53 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,14 @@
+apt install yt-dlp python3
+pip install beautifulsoup4 pyfzf plumbum requests
+mkdir bin channels config .tmp
+mv excode.py channels
+mv *.txt config
+mv backup.sh bin
+mv channel.py bin
+mv chmirror.py bin
+mv delchan.py bin
+mv linkage.py bin
+mv restore.sh bin
+mv search-channel.py bin
+mv search.py bin
+mv search_m.py bin
